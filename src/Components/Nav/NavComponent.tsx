727d59555eebb5eb3c995d784bc004ac99f4ebe4
Tiago Campêlo <tscs2@cin.ufpe.br>
2024-04-05 01:03:18 -0300

    feat: improve nav styles

--- src/Components/Nav/NavComponent.tsx
+++ src/Components/Nav/NavComponent.tsx
@@ -1,21 +1,53 @@
 import Tabs from '@mui/material/Tabs';
 import LinkTab from '@mui/material/Tab';
-import { useState } from 'react';
+import React from 'react';
 
 export default function NavComponent() {
 
-    const [value, setValue] = useState(0);
-    
+    const [value, setValue] = React.useState("");
+
+    function samePageLinkNavigation(
+        event: React.MouseEvent<HTMLAnchorElement, MouseEvent>,
+    ) {
+        if (
+            event.defaultPrevented ||
+            event.button !== 0 || // ignore everything but left-click
+            event.metaKey ||
+            event.ctrlKey ||
+            event.altKey ||
+            event.shiftKey
+        ) {
+            return false;
+        }
+        return true;
+    }
+
+    const handleChange = (event: React.SyntheticEvent, newValue: string) => {
+        // event.type can be equal to focus with selectionFollowsFocus.
+        if (
+            event.type !== "click" ||
+            (event.type === "click" &&
+                samePageLinkNavigation(
+                    event as React.MouseEvent<HTMLAnchorElement, MouseEvent>,
+                )
+            )
+        ) {
+            setValue(newValue);
+        }
+    };
 
     return (
-        <Tabs
-            value={value}
-            aria-label="nav tabs example"
-            role="navigation"
-        >
-            <LinkTab label="Page One" href="/search" />
-            <LinkTab label="Page Two" href="/favorite" />
-        </Tabs>
-    )
-
+        <div>
+            <Tabs
+                value={value}
+                onChange={handleChange}
+                textColor="secondary"
+                indicatorColor="secondary"
+                aria-label="secondary tabs example"
+            >
+                <LinkTab href="/search" value="search" label="Search" />
+                <LinkTab href="/favorite" value="favorite" label="Favorites" />
+            </Tabs>
+        </div>
+    );
 }
