94e7aeb2dbe2b970f103b5bf35173de15dc0f6cf
Tiago Campêlo <tscs2@cin.ufpe.br>
2024-04-06 22:05:12 -0300

    fix: Adds redirect to base path

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,26 @@
-import './App.scss';
+import "./App.scss";
 
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import SearchComponent from './Components/Search/SearchComponent';
-import NavComponent from './Components/Nav/NavComponent';
-import FavoritesComponent from './Components/Favorites/FavoritesComponent';
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import SearchComponent from "./Components/Search/SearchComponent";
+import NavComponent from "./Components/Nav/NavComponent";
+import FavoritesComponent from "./Components/Favorites/FavoritesComponent";
 
 function App() {
-  
-  return (
-    <>
-    <BrowserRouter>
-      <NavComponent />
-      <Routes>
-          <Route path="/search" element={<SearchComponent />} />
-          <Route path="/favorite" element={<FavoritesComponent />} />
-      </Routes>
-    </BrowserRouter>
-    </>
-  );
+    return (
+        <>
+            <BrowserRouter>
+                <NavComponent />
+                <Routes>
+                    <Route
+                        path='/'
+                        element={<Navigate to='/search' replace />}
+                    />
+                    <Route path='/search' element={<SearchComponent />} />
+                    <Route path='/favorite' element={<FavoritesComponent />} />
+                </Routes>
+            </BrowserRouter>
+        </>
+    );
 }
 
 export default App;
